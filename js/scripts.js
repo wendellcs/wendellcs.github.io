980574b0576c1406124ce2ac1cf8ffc48262f793
Wendell <wendellcs131@gmail.com>
2023-08-05 19:54:44 -0300

    Enviando mudanças

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,55 @@
 const containerSkills = document.querySelector('.skills-section-container')
 const containerProjects = document.querySelector('.projects-section')
 
-const homeBtn = document.querySelector('.home')
-const projectBtn = document.querySelector('.projects')
+const desktopHomeBtn = document.querySelector('.menu-btn.home')
+const desktopProjectsBtn = document.querySelector('.menu-btn.projects')
 
-homeBtn.addEventListener('click', () => {
+desktopHomeBtn.addEventListener('click', () => {
+    if (desktopProjectsBtn.classList.contains('active')) {
+        containerProjects.classList.remove('show')
+        containerSkills.classList.remove('hidden')
+        desktopProjectsBtn.classList.remove('active')
+        desktopHomeBtn.classList.add('active')
+    }
+})
+
+desktopProjectsBtn.addEventListener('click', () => {
+    if (desktopHomeBtn.classList.contains('active')) {
+        containerSkills.classList.add('hidden')
+        containerProjects.classList.add('show')
+        desktopHomeBtn.classList.remove('active')
+        desktopProjectsBtn.classList.add('active')
+    }
+})
+
+const mobileHomeBtn = document.querySelector('.home')
+const mobileProjectsBtn = document.querySelector('.projects')
+
+mobileHomeBtn.addEventListener('click', () => {
     if (containerProjects.classList.contains('show')) {
         containerProjects.classList.remove('show')
         containerSkills.classList.remove('hidden')
-        homeBtn.classList.add('active')
-        projectBtn.classList.remove('active')
-    } else {
-        containerSkills.classList.remove('hidden')
-        homeBtn.classList.add('active')
+        mobileHomeBtn.classList.add('active')
+        mobileProjectsBtn.classList.remove('active')
     }
-
 })
 
-projectBtn.addEventListener('click', () => {
+mobileProjectsBtn.addEventListener('click', () => {
     if (!containerSkills.classList.contains('hidden')) {
         containerSkills.classList.add('hidden')
         containerProjects.classList.add('show')
-        homeBtn.classList.remove('active')
-        projectBtn.classList.add('active')
-    } else {
-        containerProjects.classList.add('show')
-        projectBtn.classList.add('active')
+        mobileHomeBtn.classList.remove('active')
+        mobileProjectsBtn.classList.add('active')
     }
 })
+
+const mobileMenu = document.querySelector('.mobile-menu')
+const mobileMenuBtn = document.querySelector('.mobile-menu-icon')
+const mobileMenuBtnContainer = document.querySelector('.buttons-container')
+mobileMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active')
+    mobileMenuBtn.classList.toggle('active')
+    mobileMenuBtnContainer.classList.toggle('active')
+})
+
+
